54801c49dddd08e80783b54dbba4666266058895
Adnan <adnan.hammoud@inmind.ai>
2026-01-13 14:57:35 +0200

    Define order services

--- OrderManagementApp.Application/Services/OrderServices.cs
+++ OrderManagementApp.Application/Services/OrderServices.cs
@@ -1,6 +1,21 @@
-﻿namespace OrderManagementApp.Application.Services;
+﻿using OrderManagementApp.Application.Repositories;
+using OrderManagementApp.DTOs;
+using OrderManagementApp.Models;
+
+namespace OrderManagementApp.Application.Services;
 
 public class OrderServices
 {
-    
+    private readonly IOrderRepository _orderRepository;
+
+    public OrderServices(IOrderRepository orderRepository)
+    {
+        _orderRepository = orderRepository;
+    }
+    public Order CreateOrderAsync(OrderDTO orderDto)
+    {
+        Order order = _orderRepository.CreateOrder(orderDto);
+        order.Status = "Pending";
+        return order;
+    }
 }
